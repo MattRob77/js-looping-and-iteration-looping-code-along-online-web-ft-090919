573c6162c26cffda94aa5db372215d89a6415926
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-10 18:18:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Code your solutions in this file
-
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYous;
+};
