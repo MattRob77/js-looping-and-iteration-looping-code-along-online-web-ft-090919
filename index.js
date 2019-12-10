f8e322bf5ab94624685eee97af4f7dd19cb51c32
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-10 18:19:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-
+let thankYous = [];
 function writeCards(names, event) {
     for (let i = 0; i < names.length; i++) {
         thankYous.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
